35f711653843a9001def8040b59011123351e5d2
Your Name <your@example.com>
2016-05-14 16:57:34 -0400

    PrintTriangle_Initial

---
+++ Print_A_Triangle Chapt 12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_A_Triangle_Chapt_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 5;
+            for(int i =0; i < rows; i ++)
+            {
+                for (int k = rows - i - 1; k >= 0; k--)
+                    Console.Write(" ");
+
+                for (int j = 0; j <=i; j++)
+                {
+                    Console.Write(" *");
+                }
+
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
